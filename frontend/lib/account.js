97f151625a01931cd0d0e8af5ae2e5fe513f4981
Sanket <mediawale2023@gmail.com>
2026-06-08 13:29:53 +0530

    Set aspirant palette to charcoal

--- frontend/lib/account.js
+++ frontend/lib/account.js
@@ -62,7 +62,7 @@ export function getSidebarTheme(user) {
 
     let background = '#003B2A';
     if (accountStage === 'aspirant') {
-        background = '#0B3C5D';
+        background = '#242424';
     } else if (seatType === 'mp' && house === 'Rajya Sabha') {
         background = '#800000';
     }
